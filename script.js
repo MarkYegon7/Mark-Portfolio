e5e945bafacfb11151adb659875194885ade9ce3
DamianDib9 <121461459+DamianDib9@users.noreply.github.com>
2023-07-24 11:50:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburgerMenu = document.querySelector('.image-icon');
 const desktopMenu = document.querySelector('.name-1');
 const clickButton = document.querySelector('#close_button');
-// const navigationNav = document.querySelectorAll('.mobile-Menu-list > li> a');
+const navigationNav = document.querySelectorAll('.mobile-Menu-list > li> a');
 
 function clickOn() {
   desktopMenu.style.display = 'flex';
@@ -13,4 +13,4 @@ function closeMenu() {
 
 hamburgerMenu.addEventListener('click', clickOn);
 clickButton.addEventListener('click', closeMenu);
-// navigationNav.forEach((each) => each.addEventListener('click', closeMenu));
+navigationNav.forEach((each) => each.addEventListener('click', closeMenu));
